a68678d39e1572157478041988311887e3015f9b
crowncoding <padamskc@gmail.com>
2018-11-16 23:35:36 -0600

    initial wp commit; does NOT include uploads folder AT ALL.

---
+++ wp-content/themes/simplegridtheme/footer.php
@@ -0,0 +1,86 @@
+    </div><!--//content_container-->
+
+    
+
+    <div id="footer">
+
+        <div class="footer_widgets_cont">
+
+        
+
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer') ) : ?>       
+
+        
+
+            <div class="footer_box">
+
+                <h3>Widget Footer</h3>
+
+                <p>Please use widget to setup this text box. Please use widget to setup this text box. Please use widget to setup this text box. Please use widget to setup this text box.</p>
+
+            </div><!--//footer_box-->
+
+            
+
+            <div class="footer_box">
+
+                <h3>Widget Footer</h3>
+
+                <p>Please use widget to setup this text box. Please use widget to setup this text box. Please use widget to setup this text box. Please use widget to setup this text box.</p>
+
+            </div><!--//footer_box-->
+
+
+
+            
+
+            <?php endif; ?>                
+
+            
+
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer Last') ) : ?>       
+
+            
+
+            <div class="footer_box footer_box_last">
+
+                <h3>Widget Footer</h3>
+
+                <p>Please use widget to setup this text box. Please use widget to setup this text box. Please use widget to setup this text box. Please use widget to setup this text box.</p>
+
+            </div><!--//footer_box-->            
+
+            
+
+            <?php endif; ?>                
+
+            
+
+            <div class="clear"></div>
+
+        
+
+        </div><!--//footer_widgets_cont-->
+
+    
+
+
+    </div><!--//footer-->
+
+
+
+</div><!--//main_container-->
+
+
+
+<?php wp_footer(); ?>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-7584656919940117",
+    enable_page_level_ads: true
+  });
+</script>
+</body>
+
+</html>

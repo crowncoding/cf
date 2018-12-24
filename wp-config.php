0c532c435859b16812d2d7dde71ff1e72ef927f1
crowncoding <padamskc@gmail.com>
2018-12-23 23:33:27 -0600

    modifying wp-config for mamp

--- wp-config.php
+++ wp-config.php
@@ -1,2 +1,15 @@
 <?php
-include('/var/www/vhosts/celebsfeetandsoles.com/wp-config.php');
+if ($_SERVER['HTTP_HOST'] == 'cf') {
+    define('DB_NAME', 'celebsfeet');
+    define('DB_USER', 'celebsfeet');
+    define( 'WP_HOME', 'https://cf:8890' );
+    define( 'WP_SITEURL', 'https://cf:8890' );
+    define('DB_HOST', 'localhost');
+    define('DB_PASSWORD', 'Cummy_Soles1!');
+    define('DB_CHARSET', 'utf8mb4');
+    define('DB_COLLATE', '');
+} else if ($_SERVER['HTTP_HOST'] ==  'dev.celebsfeetandsoles.com'){
+    include('/var/www/vhosts/celebsfeetandsoles.com/wp-config.php');
+} else if ($_SERVER['HTTP_HOST'] ==  'celebsfeetandsoles.com'){
+    include('/var/www/vhosts/celebsfeetandsoles.com/wp-config.php');
+}
